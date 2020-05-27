8d0c63e63eadf8616f24f4a39b13406f1cdcb54c
Gustavo Decker Zambrano <gustavdz@gmail.com>
2020-05-27 18:50:00 -0500

    recepción de html para envio

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 var nodemailer = require('nodemailer');
 var express = require('express');
+const cors = require('cors');
+var bodyParser = require('body-parser');
 var app = express();
 var sesTransport = require('nodemailer-ses-transport');
 var dotenv = require('dotenv').config();
 
+//CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); //cualquier frente puede hacer peticiones ajax
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Allow', '*');
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// 1
+app.use(bodyParser.urlencoded({ extended: true }));
+// 2
+app.use(bodyParser.json());
+
+
+//rutas
 app.post("/send-email", (req, res) => {
+  var params = req.body;
 
   var mailOptions = {
     from: process.env.FROM,
     to: process.env.TO,
     text: 'This is some text',
-    html: '<p>This is some HTML</p>',
-    subject: 'Nodemailer SMTP Subject8',
+    // html: '<p>This is some HTML</p>',
+    html: params.mensaje,
+    subject: 'First HTML Newsletter',
     list: {
         unsubscribe: {
-            url: `${process.env.UNSUBSCRIBEURL}`
+            url: `${process.env.UNSUBSCRIBEURL}${process.env.TO}`
         }
     }
   };
